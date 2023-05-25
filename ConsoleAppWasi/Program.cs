858965932097c884d4c2540bc206b5b516f548f3
Niels Tanis <nielstanis@live.nl>
2023-05-25 10:33:52 +0200

    Slides PDF

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello Code Europe 2023 from {RuntimeInformation.OSArchitecture}!");
+
+var file = System.IO.File.ReadAllText("/etc/hosts");
+Console.WriteLine(file);
